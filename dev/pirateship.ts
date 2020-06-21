e4e5b8a0d555109de7a9acc96d213acc724952ad
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-06-21 18:22:40 +0200

    homework done!

--- dev/pirateship.ts
+++ dev/pirateship.ts
@@ -28,10 +28,13 @@ class PirateShip extends Ship implements Observer {
         //toggle the boolean
         this.ShouldNotify = !this.ShouldNotify
 
+        // if this.shouldnotify = true, it would register the subject
         if (this.ShouldNotify) {
             this.subject.register(this);
             this.style.backgroundImage = `url(images/ship4.png)`
         }
+        //if this should notify = false, it would unregister the subject; this won't bug because by clicking the ship once, it would already trigger the boolean to be true
+        //thus making it able to compare the variable with the boolean ;) 
         if (this.ShouldNotify == false) {
             this.style.backgroundImage = `url(images/ship-unregistered.png)`
             this.subject.unregister(this)
